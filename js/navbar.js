2fe845aad57a3d045a1ac733c438ea562aafddbf
PoQuatre <leflem.mateo2002@gmail.com>
2022-01-13 17:16:28 +0100

    Added hamburger menu for mobile

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,44 @@
 const navbar = document.getElementById("navbar");
+const menuBtn = document.getElementById("menu");
+
+if (window.scrollY === 0) {
+  navbar.classList.remove("scrolled");
+} else {
+  navbar.classList.add("scrolled");
+}
 
 window.addEventListener("scroll", () => {
   if (window.scrollY === 0) {
-    navbar.style.backgroundColor = "transparent";
+    navbar.classList.remove("scrolled");
   } else {
-    navbar.style.backgroundColor = "var(--dark)";
+    navbar.classList.add("scrolled");
   }
 });
+
+window.addEventListener(
+  "focus",
+  (e) => {
+    if (e.target === window || !menuBtn.contains(e.target)) {
+      if (e.target !== window && navbar.contains(e.target)) {
+        navbar.classList.add("open");
+      } else {
+        navbar.classList.remove("open");
+      }
+    }
+  },
+  true
+);
+
+window.addEventListener(
+  "click",
+  (e) => {
+    if (e.target !== window && menuBtn.contains(e.target)) {
+      navbar.classList.toggle("open");
+    } else if (e.target !== window && navbar.contains(e.target)) {
+      navbar.classList.add("open");
+    } else {
+      navbar.classList.remove("open");
+    }
+  },
+  true
+);
